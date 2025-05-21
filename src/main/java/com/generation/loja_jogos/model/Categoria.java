db946fc0af5a48b3b9bf7191e6b19c771c3f4e8e
angelokapunda <angelokapunda@gmail.com>
2025-05-21 10:23:25 -0300

    :sparkles: Implementação da configuração do Bando de dados

--- src/main/java/com/generation/loja_jogos/model/Categoria.java
+++ src/main/java/com/generation/loja_jogos/model/Categoria.java
@@ -1,11 +1,20 @@
 package com.generation.loja_jogos.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -15,7 +24,12 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of = "id")
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String descricao;
+
+    @OneToMany(mappedBy = "categoria", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<Produto> produtos = new ArrayList<>();
 }
--- src/main/java/com/generation/loja_jogos/model/Produto.java
+++ src/main/java/com/generation/loja_jogos/model/Produto.java
@@ -1,9 +1,14 @@
 package com.generation.loja_jogos.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -30,5 +35,8 @@ public class Produto {
 
     private BigDecimal preco;
     private String foto;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 }
