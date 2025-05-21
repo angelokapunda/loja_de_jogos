694e66c4f574ab52d1e4d42a854152c8742feeb6
angelokapunda <angelokapunda@gmail.com>
2025-05-21 10:42:46 -0300

    :sparkles: Implementação das classes de serviço(Categotia e Produto)

--- src/main/java/com/generation/loja_jogos/service/CadastroCategoria.java
+++ src/main/java/com/generation/loja_jogos/service/CadastroCategoria.java
@@ -1,7 +1,34 @@
 package com.generation.loja_jogos.service;
 
+import com.generation.loja_jogos.model.Categoria;
+import com.generation.loja_jogos.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CadastroCategoria {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria save (Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    public List<Categoria> getAll() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria getId(Long id) {
+        return categoriaRepository.findById(id).orElseThrow();
+    }
+
+    public void delete(Long id) {
+        var categoriaId = getId(id);
+        if (categoriaId != null) {
+            categoriaRepository.delete(categoriaId);
+        }
+    }
 }
--- src/main/java/com/generation/loja_jogos/service/CadastroProduto.java
+++ src/main/java/com/generation/loja_jogos/service/CadastroProduto.java
@@ -1,7 +1,46 @@
 package com.generation.loja_jogos.service;
 
+import com.generation.loja_jogos.model.Produto;
+import com.generation.loja_jogos.repository.CategoriaRepository;
+import com.generation.loja_jogos.repository.ProdutoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class CadastroProduto {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Transactional
+    public Produto save (Produto produto) {
+        if (!categoriaRepository.existsById(produto.getCategoria().getId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A Categoria não existe");
+        }
+        return produtoRepository.save(produto);
+    }
+
+    public List<Produto> getAll () {
+        return produtoRepository.findAll();
+    }
+
+    public Produto getId(Long id) {
+        return produtoRepository.findById(id).orElseThrow();
+    }
+
+    public void delete(Long id) {
+        var produtoId = getId(id);
+        if (produtoId != null) {
+            produtoRepository.delete(produtoId);
+        }
+    }
+
 }
