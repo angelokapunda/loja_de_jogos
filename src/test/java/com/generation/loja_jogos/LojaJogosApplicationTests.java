74f9216a93d5e38fd2a053d62991160c6b4c5dd3
angelokapunda <angelokapunda@gmail.com>
2025-05-21 09:18:09 -0300

    :tada: Commit inicial - Loja de Jogos

---
+++ src/test/java/com/generation/loja_jogos/LojaJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.loja_jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
